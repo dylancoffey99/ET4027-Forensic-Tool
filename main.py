b6a03c554438422a2c495d67f6e0d3306387eec0
dylancoffey99 <dylancoffey99@gmail.com>
2022-03-04 00:50:46 +0000

    Changed wording of initial input of main.py. Added new input to continue while loop to input another image. Added print for tool name/student name/student ID in the beginning.

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from os import path
 from reader import Reader
 
 if __name__ == "__main__":
+    print("ET4027 FORENSIC TOOL - PHASE 1\nNAME = Dylan Coffey\nID = 18251382\n")
     while True:
-        image_path = input("Please enter path of file system image:")
+        image_path = input("Please drop an image into the terminal or type its path:")
         if not path.exists(image_path):
             print("\nError: image path does not exist!\n")
         else:
             reader = Reader()
             reader.read_partitions(image_path)
             reader.print_partitions()
-            break
+            input("\nPress <ENTER> to input another image:\n")
