d21d1e64a8f32db1961058f7120efaeb8d70bf60
dylancoffey99 <dylancoffey99@gmail.com>
2022-04-10 07:24:44 +0100

    Added check for a path argument if main.py is run through a terminal. Created main() method to hold the implementation to run the tool. These changes allow for an executable of the tool to be run through a terminal (with an image path argument) or by itself.

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from os import path
+from sys import argv
 
 from tool import Tool
 
+
+def main():
+    tool = Tool(image_path)
+    tool.read_partitions()
+    tool.print_partitions()
+
+
 if __name__ == "__main__":
     print("ET4027 FORENSIC TOOL - PHASE 2\nNAME = Dylan Coffey\nID = 18251382\n")
-    while True:
-        image_path = input("Please drop an image into the terminal or type its path:")
-        image_path = image_path.replace('"', "")
-        if not path.exists(image_path):
-            print("\nError: image path does not exist!\n")
-        else:
-            tool = Tool(image_path)
-            tool.read_partitions()
-            tool.print_partitions()
-            input("\nPress <ENTER> to input another image:\n")
+    if len(argv) > 1:
+        image_path = argv[1]
+        main()
+    else:
+        while True:
+            image_path = input("Please drop an image into the terminal or type its path:")
+            image_path = image_path.replace('"', "")
+            if not path.exists(image_path):
+                print("\nError: image path does not exist!\n")
+            else:
+                main()
+                input("\nPress <ENTER> to input another image:\n")
